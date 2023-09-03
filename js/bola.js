02addf93b79368eaf2079f59c0ef16ca84c84574
VictorRobb <victorescdiaz4@gmail.com>
2023-09-02 23:03:59 -0600

    finalizando
    agregamos pequeños detalles

--- js/bola.js
+++ js/bola.js
@@ -1,3 +1,7 @@
 document.getElementById("btnpagar").addEventListener("click",()=>{
-    
+    window.location.href = "./formasdepago.html";
 });
+document.getElementById("pagado").addEventListener("click",()=>{
+    window.location.href = "./comprados.html";
+});
+//jejejej, no hay tiempo de meter mas js
